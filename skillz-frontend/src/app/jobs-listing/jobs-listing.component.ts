dc84b47a2b0c5228c0c3d2690e31658e49feecfd
cristianiliepaul2003@yahoo.com <cristianiliepaul2003@yahoo.com>
2024-01-13 19:27:00 +0200

    gata

--- skillz-frontend/src/app/jobs-listing/jobs-listing.component.ts
+++ skillz-frontend/src/app/jobs-listing/jobs-listing.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrl: './jobs-listing.component.css'
 })
 export class JobsListingComponent {
+  jobs = [
+    {
+        title: 'Gardener',  
+        rating: '4.3/5',
+        username: 'Gombos Andrei',
+        experience: '6+ years',
+        certificationStatus: 'Not Certified',
+        backgroundImage: "https://img.freepik.com/premium-photo/wooden-garden-table-with-trees-flowers-blurred-background-generative-ai_74760-571.jpg",
+        profileImage: 'https://assets-global.website-files.com/65635da09dc83a90af8638e0/65635da09dc83a90af863905_gardener-job-description-1659711272.webp',
+        jobTypeLogo:'https://assets-global.website-files.com/65635da09dc83a90af8638e0/6563938b39802fe6b6fd8969_gardening.png',
+        isVerified: false // Set the verified status based on your logic
 
+    }
+];
+
+ 
 }
